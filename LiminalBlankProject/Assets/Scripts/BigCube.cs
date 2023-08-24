e71e59d74e347471da666702d93c3414f82d2252
DanielRHolmesglen <daniel.roberts@holmesglen.edu.au>
2023-08-24 11:10:08 +1000

    project settings fixed and interactable objects added for testing

---
+++ LiminalBlankProject/Assets/Scripts/BigCube.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class BigCube : MonoBehaviour, IPointerClickHandler,
+    IPointerEnterHandler, IPointerExitHandler, IPointerUpHandler, IPointerDownHandler 
+{
+    public UnityEvent OnInteraction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        OnInteraction.Invoke();
+        transform.Rotate(0, 30, 0);
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.localScale *= 1.2f;
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.localScale *= 0.8f;
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        transform.localScale *= 2;
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        transform.localScale *= 0.5f;
+    }
+}
---
+++ LiminalBlankProject/Assets/Scripts/UniversalInteraction.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+//[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Rigidbody))]
+
+public class UniversalInteraction : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
+{
+    public UnityEvent OnInteraction;
+    public UnityEvent OnPress;
+    public UnityEvent OnRelease;
+    public UnityEvent OnHover;
+    public UnityEvent OnLeave;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        OnInteraction.Invoke();
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        OnPress.Invoke();
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        OnRelease.Invoke();
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        OnHover.Invoke();
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        OnLeave.Invoke();
+    }
+}
